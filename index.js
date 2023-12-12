8d3e3c18cdc7c20e70a0dc55cf3bf5f074b272a0
Samuel Omoding <samuel.omoding@student.moringaschool.com>
2023-12-12 17:31:46 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,19 @@
+const divide = function() {
+    return (2000/100);
+}
+
+const square = (x) => {
+   
+    return (x * x);
+}
+
+const add = (a, b) => {
+   
+    return (a + b);
+}
+
+module.exports = {
+    divide,
+    square,
+    add
+};
